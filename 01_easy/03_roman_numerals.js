3bb430bbe9445507936fd3b5170a4ef99fac504d
Darren Churchill <darrenmchurchill@gmail.com>
2024-03-17 14:15:12 -0700

    Add 03 Roman Numerals stub and provided tests

---
+++ 01_easy/03_roman_numerals.js
@@ -0,0 +1,18 @@
+/**
+ * Launch School Exercises
+ * JavaScript Challenges
+ * Easy Challenges
+ *
+ * Roman Numerals
+ *
+ * https://launchschool.com/exercises/117c3255
+ */
+"use strict";
+
+class RomanNumeral {
+  constructor() {
+
+  }
+}
+
+module.exports = RomanNumeral;
