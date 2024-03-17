3466c0b292405fd8897be9628578056d69a761d2
Darren Churchill <darrenmchurchill@gmail.com>
2024-03-17 13:46:57 -0700

    Add 02 Point Mutations stub and provided tests

---
+++ 01_easy/02_point_mutations.js
@@ -0,0 +1,18 @@
+/**
+ * Launch School Exercises
+ * JavaScript Challenges
+ * Easy Challenges
+ *
+ * Point Mutations
+ *
+ * https://launchschool.com/exercises/fd2aaf29
+ */
+"use strict";
+
+class DNA {
+  constructor() {
+
+  }
+}
+
+module.exports = DNA;
