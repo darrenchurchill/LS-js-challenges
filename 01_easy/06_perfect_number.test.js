8d3bdbeb9d0e2f3ea229cc54f3067caf3c121ad6
Darren Churchill <darrenmchurchill@gmail.com>
2024-03-21 11:17:24 -0700

    Add tests covering more invalid input

--- 01_easy/06_perfect_number.test.js
+++ 01_easy/06_perfect_number.test.js
@@ -1,10 +1,26 @@
+/* eslint-disable max-lines-per-function */
 "use strict";
 
 let PerfectNumber = require('./06_perfect_number.js');
 
 describe("PerfectNumber", () => {
+  test("non-number raises error", () => {
+    expect(() => PerfectNumber.classify("string")).toThrow();
+    expect(() => PerfectNumber.classify({})).toThrow();
+    expect(() => PerfectNumber.classify([])).toThrow();
+  });
+
   test("negative raises error", () => {
     expect(() => PerfectNumber.classify(-1)).toThrow();
+  });
+
+  test("zero raises error", () => {
+    expect(() => PerfectNumber.classify(0)).toThrow();
+  });
+
+  test("non-integer raises error", () => {
+    expect(() => PerfectNumber.classify(1.2)).toThrow();
+    expect(() => PerfectNumber.classify(12.00001)).toThrow();
   });
 
   test("classify deficient", () => {
