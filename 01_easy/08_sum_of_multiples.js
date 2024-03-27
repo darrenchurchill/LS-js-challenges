7043b2dbf40440a4cbe05c52202fb6b542f31808
Darren Churchill <darrenmchurchill@gmail.com>
2024-03-27 09:07:13 -0700

    Add 08 Sum of Multiples and provided tests

---
+++ 01_easy/08_sum_of_multiples.js
@@ -0,0 +1,52 @@
+/**
+ * Launch School Exercises
+ * JavaScript Challenges
+ * Easy Challenges
+ *
+ * Sum of Multiples
+ *
+ * https://launchschool.com/exercises/89734a2f
+ */
+"use strict";
+
+class SumOfMultiples {
+  /**
+   * Create a new `SumOfMultiples` instance, given 1 or more positive integer
+   * divisors.
+   * @param  {...number} divisors 1 or more divisors to use when calculating the
+   * sum. Default to 3 and 5.
+   */
+  constructor(...divisors) {
+    this.divisors = divisors.length > 0 ? divisors : [3, 5];
+  }
+
+  /**
+   * Given a positive integer `targetValue`, return the sum of this object's
+   * divisors' multiples < `targetValue`.
+   * @param {number} targetValue the number to calculate multiples up to
+   * @returns {number} the calculated sum of multiples
+   */
+  to(targetValue) {
+    let result = new Set();
+
+    for (let count = 0; count < targetValue; count += 1) {
+      this.divisors.forEach((divisor) => {
+        if (count % divisor === 0) result.add(count);
+      });
+    }
+
+    return [...result].reduce((sum, multiple) => sum + multiple, 0);
+  }
+
+  /**
+   * Given a positive integer `targetValue`, return the sum of the default
+   * divisors' (3 and 5) multiples < `targetValue`.
+   * @param {number} targetValue the number to calculate multiples up to
+   * @returns {number} the calculated sum of multiples
+   */
+  static to(targetValue) {
+    return (new SumOfMultiples().to(targetValue));
+  }
+}
+
+module.exports = SumOfMultiples;
